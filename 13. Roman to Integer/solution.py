e4e0d5b9d21f2902e96a5dd5d19d3eb4eea1cf4e
bigjack <bigjackCN@gmail.com>
2024-01-08 01:18:36 -0500

    Leetcode 00013 Roman to Integer

--- 13. Roman to Integer/solution.py
+++ 13. Roman to Integer/solution.py
@@ -2,7 +2,7 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         roman_numeral = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         result = 0
-        last = 0    # use last to keep check of previous value
+        last = 0    # use last to check for previous value
         for symbol in s:
             result = result + roman_numeral[symbol]
             if roman_numeral[symbol] <= last:
